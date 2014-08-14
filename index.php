0244155d459480d8b099eb78033db9d11ce63b65
iftakharul alam <iftakharul.alam@live.com>
2014-08-14 10:10:23 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,225 @@
+<html>
+
+    <head>
+        <title></title>
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/personal.css">
+
+    </head>
+    <body>
+
+    <nav class="navbar navbar-inverse" role="navigation">
+        <div class="my_container">
+
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Student Course Assignment</a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Dashboard</a></li>
+
+                </ul>
+            </div>
+
+            <div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <span class="glyphicon glyphicon-user"></span>
+                             Administrator <b class="caret"></b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Settings</a></li>
+                            <li><a href="#">Logout</a></li>
+
+                        </ul>
+                    </li>
+                </ul>
+                <form class="navbar-form navbar-right" role="search">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+        <div class="container my_container">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <h2>Student List</h2>
+                        </div>
+                        <div class="col-lg-offset-1"></div>
+                        <div class="col-lg-4">
+                            <h3 class="btn btn-lg btn-danger btn-block">Add New Student</h3>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="panel-body">
+
+                    <table class="table table-bordered">
+
+                        <thead class="panel panel-default">
+                        <tr class="panel-heading">
+                            <th class="col-lg-7 text-center">Student Name</th>
+                            <th class="text-center">Operation</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        <tr>
+                            <td>
+                                <ul class="nav nav-pills nav-stacked">
+                                    <li><a href="#">Faysal Hasan</a></li>
+                                </ul>
+                            </td>
+                            <td>
+                                <div class="btn-toolbar" role="toolbar">
+
+                                    <div class="btn-group btn-group-lg">
+                                        <button type="button" class="btn btn-info">Add</button>
+                                        <button type="button" class="btn btn-info">Edit</button>
+                                        <button type="button" class="btn btn-info">Update</button>
+                                        <button type="button" class="btn btn-info">Delete</button>
+                                    </div>
+
+                                </div>
+                            </td>
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <ul class="nav nav-pills nav-stacked">
+                                    <li><a href="#">Iftakharul Alam</a></li>
+                                </ul>
+                            </td>
+                            <td>
+                                <div class="btn-toolbar" role="toolbar">
+
+                                    <div class=" btn-group btn-group-lg">
+                                        <button type="button" class="btn btn-info">Add</button>
+                                        <button type="button" class="btn btn-info">Edit</button>
+                                        <button type="button" class="btn btn-info">Update</button>
+                                        <button type="button" class="btn btn-info">Delete</button>
+                                    </div>
+
+                                </div>
+                            </td>
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <ul class="nav nav-pills nav-stacked">
+                                    <li><a href="#">Ariful Islam</a></li>
+                                </ul>
+                            </td>
+                            <td>
+                                <div class="btn-toolbar" role="toolbar">
+
+                                    <div class=" btn-group btn-group-lg">
+                                        <button type="button" class="btn btn-info">Add</button>
+                                        <button type="button" class="btn btn-info">Edit</button>
+                                        <button type="button" class="btn btn-info">Update</button>
+                                        <button type="button" class="btn btn-info">Delete</button>
+                                    </div>
+
+                                </div>
+                            </td>
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <ul class="nav nav-pills nav-stacked">
+                                    <li><a href="#">Kaisar Hamid</a></li>
+                                </ul>
+                            </td>
+                            <td>
+                                <div class="btn-toolbar" role="toolbar">
+
+                                    <div class=" btn-group btn-group-lg">
+                                        <button type="button" class="btn btn-info">Add</button>
+                                        <button type="button" class="btn btn-info">Edit</button>
+                                        <button type="button" class="btn btn-info">Update</button>
+                                        <button type="button" class="btn btn-info">Delete</button>
+                                    </div>
+
+                                </div>
+                            </td>
+
+                        </tr>
+
+                        <tr>
+                            <td>
+                                <ul class="nav nav-pills nav-stacked">
+                                    <li><a href="#">Mainul Islam</a></li>
+                                </ul>
+                            </td>
+                            <td>
+                                <div class="btn-toolbar" role="toolbar">
+
+                                    <div class=" btn-group btn-group-lg">
+                                        <button type="button" class="btn btn-info">Add</button>
+                                        <button type="button" class="btn btn-info">Edit</button>
+                                        <button type="button" class="btn btn-info">Update</button>
+                                        <button type="button" class="btn btn-info">Delete</button>
+                                    </div>
+
+                                </div>
+                            </td>
+
+                        </tr>
+
+                        </tbody>
+                    </table>
+
+
+                </div>
+
+                <!--
+                 <div class="row">
+                        <div class="col-lg-6">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    Student List
+                                </div>
+                                <div class="panel-body">
+
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-offset-1">
+
+                        </div>
+
+                        <div class="col-lg-5">
+
+                        </div>
+                    </div>
+                -->
+
+                <div class="panel-footer text-right">
+
+                        Student Registration 2014
+
+                </div>
+
+            </div>
+        </div>
+
+    <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </body>
+</html>
