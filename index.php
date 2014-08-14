87f22b45872c9b22f773b1b8e221235bb6c04f32
iftakharul alam <iftakharul.alam@live.com>
2014-08-14 13:01:46 +0600

    add student added

--- index.php
+++ index.php
@@ -1,83 +1,107 @@
 <html>
 
-    <head>
-        <title></title>
+<head>
+    <title></title>
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/personal.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/personal.css">
 
-    </head>
-    <body>
+</head>
+<body>
 
-    <nav class="navbar navbar-default" role="navigation">
+<nav class="navbar navbar-default" role="navigation">
 
-        <div class="my_container">
+    <div class="my_container">
 
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">Student Course Assignment</a>
-            </div>
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Student Course Assignment</a>
+        </div>
 
-            <div>
-                <ul class="nav navbar-nav">
+        <div>
+            <ul class="nav navbar-nav">
 
-                    <li class="active"><a href="#">
-                            <span class="glyphicon glyphicon-list-alt"></span>
-                            &nbsp; Dashboard</a></li>
+                <li class="active"><a href="#">
+                        <span class="glyphicon glyphicon-list-alt"></span>
+                        &nbsp; Dashboard</a></li>
 
-                </ul>
-            </div>
+            </ul>
+        </div>
 
-            <div>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <span class="glyphicon glyphicon-user"></span>
-                             Administrator <b class="caret"></b>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#"><span class="glyphicon glyphicon-wrench"></span> &nbsp; Settings</a></li>
-                            <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> &nbsp; Logout</a></li>
+        <div>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <span class="glyphicon glyphicon-user"></span>
+                        Administrator <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><span class="glyphicon glyphicon-wrench"></span> &nbsp; Settings</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> &nbsp; Logout</a></li>
 
-                        </ul>
-                    </li>
-                </ul>
-                <form class="navbar-form navbar-right" role="search">
-                    <div class="form-group has-feedback has-feedback-left">
-                        <span class="form-control-feedback glyphicon glyphicon-search"></span>
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                </form>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group has-feedback has-feedback-left">
+                    <span class="form-control-feedback glyphicon glyphicon-search"></span>
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+            </form>
 
+        </div>
+
+    </div>
+
+</nav>
+
+<div class="container my_container">
+
+    <div class="panel panel-info">
+
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h2><span class="glyphicon glyphicon-list"></span> Dashboard</h2>
+                </div>
+                <div class="col-lg-offset-1"></div>
+                <div class="col-lg-4">
+                    <a href="index.php" class="btn btn-lg btn-danger btn-block"><span class="glyphicon glyphicon-home">
+                         &nbsp;
+                    </span>Home</a>
+                </div>
             </div>
 
         </div>
 
-    </nav>
+        <div class="panel-body">
 
-        <div class="container my_container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <a href="#" class="list-group-item active">
+                        Menu
+                    </a>
+                    <a href="index.php" class="list-group-item"><span class="badge alert-danger">Average</span>Student Status</a>
+                    <a href="add_student.html" class="list-group-item"><span class="badge alert-info"><span class="glyphicon glyphicon-plus-sign"></span></span>Add Student</a>
+                    <a href="#" class="list-group-item"><span class="badge alert-info"><span class="glyphicon glyphicon-plus-sign"></span></span>Add Course</a>
+                    <a href="#" class="list-group-item"><span class="badge alert-info"><span class="glyphicon glyphicon-plus-sign"></span></span>Add Trainer</a>
+                    <a href="#" class="list-group-item"><span class="badge alert-info"><span class="glyphicon glyphicon-align-justify"></span></span>Result</a>
+                    <a href="#" class="list-group-item"><span class="badge alert-success">88%</span>Course Status</a>
+                    <a href="#" class="list-group-item"><span class="badge">1</span>New Mail</a>
+                    <a href="#" class="list-group-item"><span class="badge">19</span> Application</a>
+                    <a href="#" class="list-group-item"><span class="badge">19</span> FeedBack</a>
+                </div>
 
-            <div class="panel panel-info">
-
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-lg-8">
-                            <h2>Student List</h2>
-                        </div>
-                        <div class="col-lg-offset-1"></div>
-                        <div class="col-lg-4">
-                            <a href="new_srudent.html" class="btn btn-lg btn-danger btn-block">Add New Student</a>
-                        </div>
-                    </div>
+                <div class="col-lg-offset-1">
 
                 </div>
 
-                <div class="panel-body">
+                <div class="col-lg-8">
 
                     <table class="table table-bordered">
 
                         <thead class="panel panel-default">
                         <tr class="panel-heading">
-                            <th class="col-lg-7 text-center">Student Name</th>
+                            <th class="col-lg-4 text-center">Student Name</th>
                             <th class="text-center">Operation</th>
 
                         </tr>
@@ -192,42 +216,26 @@
                         </tbody>
                     </table>
 
-
                 </div>
-
-                <!--
-                 <div class="row">
-                        <div class="col-lg-6">
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    Student List
-                                </div>
-                                <div class="panel-body">
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-offset-1">
-
-                        </div>
-
-                        <div class="col-lg-5">
-
-                        </div>
-                    </div>
-                -->
-
-                <div class="panel-footer text-right">
-
-                        Student Registration 2014
-
-                </div>
-
             </div>
+
+
         </div>
 
-    <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    </body>
+
+
+        <div class="panel-footer text-right">
+
+            Student Registration 2014
+
+        </div>
+
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
 </html>
+
+
